2769f47fbb04f9dd7ad56547375fc1b3f121264b
Arthur Ryoo <jene0523@gmail.com>
2021-05-18 23:40:27 -0700

    Implement CRUD for hotel

--- HotelListing/Models/HotelDTO.cs
+++ HotelListing/Models/HotelDTO.cs
@@ -26,7 +26,12 @@ namespace HotelListing.Models
         [Range(1, 5)]
         public double Rating { get; set; }
 
-        [Required]
+        //[Required]
         public int CountryId { get; set; }
+    }
+
+    public class UpdateHotelDTO : CreateHotelDTO
+    {
+
     }
 }
